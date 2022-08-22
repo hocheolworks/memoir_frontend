a796dc3e0e993c4152b1bd81f93cf80873cdd2bd
lhjeong60 <lhjeong60@naver.com>
2022-08-22 22:24:00 +0900

    style: theme 폴더 삭제
    1. theme 폴더 삭제
    2. 필요없음

--- theme/reactive.ts
+++
@@ -1,2 +0,0 @@
-export const HorizontalReactiveLayout =
-  "w-full ml-auto mr-auto pl-4 pr-4 first:w-1024px first:pl-0 first:pr-0 second:w-1376px second:pl-0 second:pr-0 third:w-1728px third:pl-0 third:pr-0 a";
