c1ae73614004631890110b831d042c2af5cbdf92
lhjeong60 <lhjeong60@naver.com>
2022-08-24 01:05:09 +0900

    chore: config pm2 ci/cd

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "memoir_frontend",
+      script: "main.js",
+    },
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "54.180.51.124",
+      path: "/home/ubuntu/memoir_frontend/",
+      repo: "git@github.com:hocheolworks/memoir_frontend.git",
+      ref: "origin/main",
+      key: "/Users/hjlee/Projects/pems/memoir_prd.pem",
+      "post-deploy":
+        "npm i; npm run build; pm2 start npm --name memoir_frontend -- start --watch;",
+    },
+  },
+};
