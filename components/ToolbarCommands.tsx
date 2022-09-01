ca84fca60af001497533f6bb9f80a37b1260e215
hjlee <hjlee@pintel.co.kr>
2022-09-01 12:59:16 +0900

    feat: toolbar command - title1 done
    - functional development done

--- components/ToolbarCommands.tsx
+++ components/ToolbarCommands.tsx
@@ -1,60 +1,75 @@
-import { ICommandBase, ICommandChildHandle } from "@uiw/react-md-editor";
-import { RiSubscript2 } from "react-icons/ri";
+import { ICommandBase, ICommandChildHandle } from '@uiw/react-md-editor';
+import { RiSubscript2 } from 'react-icons/ri';
+
+const setPrefix = (prefix: string, text: string) => {
+	return prefix + ' ' + text;
+};
 
 export const title1: ICommandBase<string> = {
-  name: "title1",
-  keyCommand: "title1",
-  render: (command, disabled, executeCommand) => {
-    return (
-      <button
-        aria-label="Insert title1"
-        disabled={disabled}
-        onClick={(evn) => {
-          evn.stopPropagation();
-          executeCommand(command, command.groupName);
-        }}
-      >
-        <RiSubscript2 size={25} />
-      </button>
-    );
-  },
-  execute: (state, api) => {
-    const text: string = state.text;
-    const cursor: number = state.selection.end;
+	name: 'title1',
+	keyCommand: 'title1',
+	render: (command, disabled, executeCommand) => {
+		return (
+			<button
+				aria-label="Insert title1"
+				disabled={disabled}
+				onClick={(evn) => {
+					evn.stopPropagation();
+					executeCommand(command, command.groupName);
+				}}>
+				<RiSubscript2 size={25} />
+			</button>
+		);
+	},
+	execute: (state, api) => {
+		const text: string = state.text;
+		const cursor: number = state.selection.end;
 
-    const leftLineBreak: number = text.lastIndexOf("\n", cursor - 1);
-    const rightLineBreak: number = text.indexOf("\n", cursor);
+		const leftLineBreakIndex: number = text.lastIndexOf('\n', cursor - 1);
+		const rightLineBreakIndex: number = text.indexOf('\n', cursor);
 
-    const newSelection = {
-      start: leftLineBreak === -1 ? 0 : leftLineBreak + 1,
-      end: rightLineBreak === -1 ? text.length : rightLineBreak,
-    };
-    console.log(state.selection);
-    console.log(newSelection);
-    console.log("-------------------------------");
+		const newSelection = {
+			start: leftLineBreakIndex === -1 ? 0 : leftLineBreakIndex + 1,
+			end: rightLineBreakIndex === -1 ? text.length : rightLineBreakIndex,
+		};
+		console.log(state.selection);
+		console.log(newSelection);
+		console.log('-------------------------------');
 
-    const newSelectedText: string =
-      api.setSelectionRange(newSelection).selectedText;
+		const newSelectedText: string =
+			api.setSelectionRange(newSelection).selectedText;
 
-    console.log(newSelectedText);
-    // const splitText: string[] = newSelectedText.split(" ");
-    // if (splitText.length > 0) {
-    //   switch (splitText[0]) {
-    //     case "#":
-    //       return;
-    //     case "##":
-    //     case "###":
-    //     case "####":
-    //     case "#####":
-    //     case "######":
-    //       console.log(splitText[0]);
-    //       splitText[0] = "#";
-    //       api.replaceSelection(splitText.join(" "));
-    //       break;
-    //     default:
-    //       api.replaceSelection("# " + newSelectedText);
-    //       break;
-    //   }
-    // }
-  },
+		console.log(newSelectedText);
+
+		const firstSpaceIndex = newSelectedText.indexOf(' ');
+
+		if (firstSpaceIndex < 1) {
+			// 해당 라인에 ' '이 없거나 ' '으로 시작하는 경우
+			api.replaceSelection(setPrefix('#', newSelectedText));
+		} else {
+			// 해당 라인에 두번째 글자부터 ' '이 하나라도 포함되는 경우
+			const firstWord = newSelectedText.slice(0, firstSpaceIndex);
+
+			switch (firstWord) {
+				case '#':
+					api.setSelectionRange({
+						start: newSelection.end,
+						end: newSelection.end,
+					});
+					return;
+				case '##':
+				case '###':
+				case '####':
+				case '#####':
+				case '######':
+					api.replaceSelection(
+						newSelectedText.replace(firstWord, '#')
+					);
+					break;
+				default:
+					api.replaceSelection(setPrefix('#', newSelectedText));
+					break;
+			}
+		}
+	},
 };
