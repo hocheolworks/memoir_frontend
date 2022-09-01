4e53be270c2d9df99ed91ed3f9983d594246c913
hjlee <hjlee@pintel.co.kr>
2022-09-01 18:29:48 +0900

    feat: toolbar command - titleN done
    - funtionalize title toolbar command to reuse

--- components/ToolbarCommands.tsx
+++ components/ToolbarCommands.tsx
@@ -5,71 +5,77 @@ const setPrefix = (prefix: string, text: string) => {
 	return prefix + ' ' + text;
 };
 
-export const title1: ICommandBase<string> = {
-	name: 'title1',
-	keyCommand: 'title1',
-	render: (command, disabled, executeCommand) => {
-		return (
-			<button
-				aria-label="Insert title1"
-				disabled={disabled}
-				onClick={(evn) => {
-					evn.stopPropagation();
-					executeCommand(command, command.groupName);
-				}}>
-				<RiSubscript2 size={25} />
-			</button>
-		);
-	},
-	execute: (state, api) => {
-		const text: string = state.text;
-		const cursor: number = state.selection.end;
+export const titleN = (N: number): ICommandBase<string> => {
+	return {
+		name: 'title' + N,
+		keyCommand: 'title' + N,
+		render: (command, disabled, executeCommand) => {
+			return (
+				<button
+					aria-label={'Insert title' + N}
+					disabled={disabled}
+					onClick={(evn) => {
+						evn.stopPropagation();
+						executeCommand(command, command.groupName);
+					}}>
+					<RiSubscript2 size={25} />
+				</button>
+			);
+		},
+		execute: (state, api) => {
+			const text: string = state.text;
+			const cursor: number = state.selection.end;
 
-		const leftLineBreakIndex: number = text.lastIndexOf('\n', cursor - 1);
-		const rightLineBreakIndex: number = text.indexOf('\n', cursor);
+			const leftLineBreakIndex: number = text.lastIndexOf(
+				'\n',
+				cursor - 1
+			);
+			const rightLineBreakIndex: number = text.indexOf('\n', cursor);
 
-		const newSelection = {
-			start: leftLineBreakIndex === -1 ? 0 : leftLineBreakIndex + 1,
-			end: rightLineBreakIndex === -1 ? text.length : rightLineBreakIndex,
-		};
-		console.log(state.selection);
-		console.log(newSelection);
-		console.log('-------------------------------');
+			const newSelection = {
+				start: leftLineBreakIndex === -1 ? 0 : leftLineBreakIndex + 1,
+				end:
+					rightLineBreakIndex === -1
+						? text.length
+						: rightLineBreakIndex,
+			};
 
-		const newSelectedText: string =
-			api.setSelectionRange(newSelection).selectedText;
+			const newSelectedText: string =
+				api.setSelectionRange(newSelection).selectedText;
 
-		console.log(newSelectedText);
+			const firstSpaceIndex = newSelectedText.indexOf(' ');
 
-		const firstSpaceIndex = newSelectedText.indexOf(' ');
+			if (firstSpaceIndex < 1) {
+				// 해당 라인에 ' '이 없거나 ' '으로 시작하는 경우
+				api.replaceSelection(setPrefix('#'.repeat(N), newSelectedText));
+			} else {
+				// 해당 라인에 두번째 글자부터 ' '이 하나라도 포함되는 경우
+				const firstWord = newSelectedText.slice(0, firstSpaceIndex);
 
-		if (firstSpaceIndex < 1) {
-			// 해당 라인에 ' '이 없거나 ' '으로 시작하는 경우
-			api.replaceSelection(setPrefix('#', newSelectedText));
-		} else {
-			// 해당 라인에 두번째 글자부터 ' '이 하나라도 포함되는 경우
-			const firstWord = newSelectedText.slice(0, firstSpaceIndex);
-
-			switch (firstWord) {
-				case '#':
-					api.setSelectionRange({
-						start: newSelection.end,
-						end: newSelection.end,
-					});
-					return;
-				case '##':
-				case '###':
-				case '####':
-				case '#####':
-				case '######':
-					api.replaceSelection(
-						newSelectedText.replace(firstWord, '#')
-					);
-					break;
-				default:
-					api.replaceSelection(setPrefix('#', newSelectedText));
-					break;
+				switch (firstWord) {
+					case '#'.repeat(N):
+						api.setSelectionRange({
+							start: newSelection.end,
+							end: newSelection.end,
+						});
+						return;
+					case '#':
+					case '##':
+					case '###':
+					case '####':
+					case '#####':
+					case '######':
+						api.replaceSelection(
+							newSelectedText.replace(firstWord, '#'.repeat(N))
+						);
+						break;
+					default:
+						api.replaceSelection(
+							setPrefix('#'.repeat(N), newSelectedText)
+						);
+						break;
+				}
 			}
-		}
-	},
+		},
+	};
 };
--- pages/write.tsx
+++ pages/write.tsx
@@ -1,28 +1,30 @@
-import { MDEditorProps } from "@uiw/react-md-editor";
-import dynamic from "next/dynamic";
-import { FC, useState } from "react";
-import {} from "@uiw/react-md-editor/lib/commands";
-import { title1 } from "../components/ToolbarCommands";
+import { MDEditorProps } from '@uiw/react-md-editor';
+import dynamic from 'next/dynamic';
+import { FC, useState } from 'react';
+import {} from '@uiw/react-md-editor/lib/commands';
+import { titleN } from '../components/ToolbarCommands';
 
-const MDEditor = dynamic<MDEditorProps>(() => import("@uiw/react-md-editor"), {
-  ssr: false,
+const MDEditor = dynamic<MDEditorProps>(() => import('@uiw/react-md-editor'), {
+	ssr: false,
 });
 
 const Write: FC = () => {
-  const [content, setContent] = useState<string | undefined>("## Hello World");
+	const [content, setContent] = useState<string | undefined>(
+		'## Hello World'
+	);
 
-  const [isDarkmode, setIsDarkmode] = useState<boolean>(true);
+	const [isDarkmode, setIsDarkmode] = useState<boolean>(true);
 
-  return (
-    <div className="h-full" data-color-mode={isDarkmode ? "dark" : "light"}>
-      <MDEditor
-        value={content}
-        onChange={setContent}
-        height={"100%"}
-        commands={[title1]}
-      />
-    </div>
-  );
+	return (
+		<div className="h-full" data-color-mode={isDarkmode ? 'dark' : 'light'}>
+			<MDEditor
+				value={content}
+				onChange={setContent}
+				height={'100%'}
+				commands={[1, 2, 3, 4, 5, 6].map((val) => titleN(val))}
+			/>
+		</div>
+	);
 };
 
 export default Write;
