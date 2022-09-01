8517a1875f65609ef6568efae066357715b84719
lhjeong60 <lhjeong60@naver.com>
2022-09-02 00:06:58 +0900

    fix: ERR_REQUIRE_ESM
    - copy origin code that occur ERR_REQUIRE_ESM in react-md-editor, paste to inside of project

--- pages/write.tsx
+++ pages/write.tsx
@@ -1,30 +1,39 @@
-import { MDEditorProps } from '@uiw/react-md-editor';
-import dynamic from 'next/dynamic';
-import { FC, useState } from 'react';
-import {} from '@uiw/react-md-editor/lib/commands';
-import { titleN } from '../components/ToolbarCommands';
+import { MDEditorProps } from "@uiw/react-md-editor";
+import dynamic from "next/dynamic";
+import { FC, useState } from "react";
+import { divider } from "@uiw/react-md-editor/lib/commands";
+import {
+  bold,
+  italic,
+  strikethrough,
+  titleN,
+} from "../components/ToolbarCommands";
 
-const MDEditor = dynamic<MDEditorProps>(() => import('@uiw/react-md-editor'), {
-	ssr: false,
+const MDEditor = dynamic<MDEditorProps>(() => import("@uiw/react-md-editor"), {
+  ssr: false,
 });
 
 const Write: FC = () => {
-	const [content, setContent] = useState<string | undefined>(
-		'## Hello World'
-	);
+  const [content, setContent] = useState<string | undefined>("## Hello World");
 
-	const [isDarkmode, setIsDarkmode] = useState<boolean>(true);
+  const [isDarkmode, setIsDarkmode] = useState<boolean>(true);
 
-	return (
-		<div className="h-full" data-color-mode={isDarkmode ? 'dark' : 'light'}>
-			<MDEditor
-				value={content}
-				onChange={setContent}
-				height={'100%'}
-				commands={[1, 2, 3, 4, 5, 6].map((val) => titleN(val))}
-			/>
-		</div>
-	);
+  return (
+    <div className="h-full" data-color-mode={isDarkmode ? "dark" : "light"}>
+      <MDEditor
+        value={content}
+        onChange={setContent}
+        height={"100%"}
+        commands={[
+          ...[1, 2, 3, 4, 5, 6].map((val) => titleN(val)),
+          divider,
+          bold,
+          italic,
+          strikethrough,
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Write;
