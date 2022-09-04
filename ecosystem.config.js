d0519d07058b39be295ffc35366841a79322e594
lhjeong60 <lhjeong60@naver.com>
2022-09-04 15:51:20 +0900

    chore: update ecosystem for env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,12 @@ module.exports = {
       cwd: "/home/ubuntu/memoir_frontend/",
       script: "npm",
       args: "start",
+      env: {
+        NODE_ENV: "production",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
   ],
   deploy: {
