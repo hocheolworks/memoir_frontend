78972efcec9653a33a44f29f49f8c443c9039b7f
lhjeong60 <lhjeong60@naver.com>
2022-09-04 16:21:25 +0900

    chore: env done

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -9,7 +9,7 @@ const Login: FC = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <a
-        href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_OAUTH_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_FRONT_URL}`}
+        href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_OAUTH_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GITHUB_OAUTH_REDIRECT_URI}`}
       >
         <LabelBtn label="Github 로그인"></LabelBtn>
       </a>
