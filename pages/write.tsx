f683259b1eaace1a8c7028479bf647bf25678133
lhjeong60 <lhjeong60@naver.com>
2022-10-19 20:05:36 +0900

    ing: 글 작성 페이지 제목 및 태그 영역 개발중

--- pages/write.tsx
+++ pages/write.tsx
@@ -19,6 +19,8 @@ const Write: NextPageWithLayout = () => {
   const [content, setContent] = useState<string | undefined>(
     "## 오늘을 기록해보세요!"
   );
+  const [title, setTitle] = useState<string>("");
+
   const { theme } = useTheme();
 
   return (
@@ -27,10 +29,18 @@ const Write: NextPageWithLayout = () => {
       data-color-mode={theme ?? "dark"}
     >
       <div className="w-1/2">
-        <div className="h-[190px]">
-          <div className="text-center">Title & Tags Area</div>
+        <div className="min-h-[190px] bg-white dark:bg-black">
+          <textarea
+            className="w-full px-4 py-4 text-5xl font-bold bg-white outline-none resize-none h-fit dark:bg-black"
+            placeholder="제목을 입력하세요"
+          />
+          <hr className="h-px mx-4 my-4 border-gray-600" />
+          <textarea
+            className="h-auto min-h-[40px] w-full resize-none overflow-visible break-words bg-white px-4 py-4 outline-none dark:bg-black"
+            placeholder="태그를 입력하세요"
+          />
         </div>
-        <div className="h-[calc(100%-190px)]">
+        <div className="h-full">
           <MDEditor
             visibleDragbar={false}
             value={content}
@@ -43,7 +53,6 @@ const Write: NextPageWithLayout = () => {
             //   italic,
             //   strikethrough,
             // ]}
-            fullscreen={false}
             extraCommands={[]}
             preview={"edit"}
           />
@@ -62,9 +71,9 @@ const Write: NextPageWithLayout = () => {
         //   italic,
         //   strikethrough,
         // ]}
-        fullscreen={false}
         extraCommands={[]}
         preview={"preview"}
+        contentEditable={"false"}
       />
     </div>
   );
