1f3cc5e7f78598848c6f3804034ba208ca8455a4
lhjeong60 <lhjeong60@naver.com>
2022-12-23 15:20:21 +0900

    ing: 마이페이지 500 에러 테스트용 수정

--- api/error.ts
+++ api/error.ts
@@ -10,7 +10,9 @@ export const errorHandler = (e: any) => {
   if (axios.isAxiosError(e) && e.response) {
     const { statusCode, message } = e.response.data as ErrorResponse; // as 안쓸 방법이 없을까..?
     console.log(`${statusCode} Error`);
-    alert(message);
+    if (typeof window !== "undefined") {
+      alert(message);
+    }
   } else {
     console.log(e);
   }
