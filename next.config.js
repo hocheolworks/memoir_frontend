db017fae8fbb6b32a5aa3aadcc7ba4e913c761ea
lhjeong60 <lhjeong60@naver.com>
2022-12-28 16:51:54 +0900

    feat: 로그인 완료페이지 로딩 표시
    - react-spinners 사용
    - code가 들어오지 않았을 때 예외처리
    - useEffect()가 두번 실행되는 것을 막기 위해 strictMode 해제

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
--- pages/login/done.tsx
+++ pages/login/done.tsx
@@ -1,26 +1,20 @@
 import { useRouter } from "next/router";
-import { FC, useEffect } from "react";
+import { ReactElement, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { errorHandler } from "../../api/error";
 import UserAPI from "../../api/user/userAPI";
-import { setAuthUser } from "../../redux/modules/authSlice";
+import { resetAuth, setAuthUser } from "../../redux/modules/authSlice";
 import { dummyUser } from "../../utils/dummy";
 import { User } from "../../utils/types";
+import { NextPageWithLayout } from "../_app";
+import { GridLoader } from "react-spinners";
 
-const LoginDone: FC = () => {
+const LoginDone: NextPageWithLayout = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const { code } = router.query;
 
   const asyncWrapper = async () => {
-    if (process.env.NODE_ENV === "development") {
-      dispatch(setAuthUser(dummyUser));
-      router.push("/");
-      return;
-    }
-
-    if (!code) return;
-
     try {
       const res = await UserAPI.login({ code: code as string });
 
@@ -38,14 +32,32 @@ const LoginDone: FC = () => {
   };
 
   useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      dispatch(setAuthUser(dummyUser));
+      router.push("/");
+      return;
+    }
+
+    if (!code) {
+      dispatch(resetAuth());
+      alert("로그인에 실패했습니다. \n 다시 시도해주세요.");
+      router.push("/");
+      return;
+    }
+
     asyncWrapper();
-  });
+  }, []);
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      Login Done
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <GridLoader loading={code !== undefined} size={"35"} color="#904CF9" />
+      <div className="text-bold text-3xl"></div>
     </div>
   );
+};
+
+LoginDone.getLayout = function getLayout(page: ReactElement) {
+  return page;
 };
 
 export default LoginDone;
